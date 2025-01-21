26a0656e6d39e6a5ef267776a31a1f5014af2806
Chief <ChiefService@outlook.com>
2025-01-22 02:33:32 +0900

    Moved social feature to the separate file

--- TLD15/TLD15/Pages/Social/PreviewSocialPartial.cshtml.cs
+++ TLD15/TLD15/Pages/Social/PreviewSocialPartial.cshtml.cs
@@ -5,49 +5,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using TLD15.Utils;
 
 namespace TLD15.Pages.Social;
-
-public sealed class PreviewSocialFeature : IFeature
-{
-    public sealed class Response
-    {
-        public required string Url { get; set; }
-        public required string Name { get; set; }
-
-        public string Image 
-        {
-            get
-            {
-                return IconHelper.GetIcon(Name);
-            }
-        }
-    }
-
-    public sealed class Request : IRequest<List<Response>> { }
-
-    public sealed class GetPreviewContactsHandler(IMongoClient client) : IRequestHandler<Request, List<Response>>
-    {
-        public async Task<List<Response>> Handle(Request request, CancellationToken cancellationToken)
-        {
-            var database = client.GetDatabase(EntitySocial.Database);
-            var collection = database.GetCollection<EntitySocial>(EntitySocial.Collection);
-
-            var documents = await collection.Find(FilterDefinition<EntitySocial>.Empty)
-                .SortByDescending(x => x.CreatedAt)
-                .ToListAsync(cancellationToken);
-
-            return documents.ConvertAll(x => new Response
-            {
-                Name = x.Name,
-                Url = x.Url,
-            });
-        }
-    }
-}
 
 public class PreviewSocialPartialModel : PageModel, IPartial
 {
@@ -55,27 +18,27 @@ public class PreviewSocialPartialModel : PageModel, IPartial
 
     public static async Task<PreviewSocialPartialModel> InitializeAsync(IMediator mediator)
     {
-        var contacts = await FeatureRunner.Run(async () =>
+        var socials = await FeatureRunner.Run(async () =>
         {
-            return await mediator.Send(new PreviewSocialFeature.Request());
+            return await mediator.Send(new ASocialFeature.RequestPreview() );
         });
 
         var result = new PreviewSocialPartialModel();
 
-        if (contacts.Incident is not null)
+        if (socials.Incident is not null)
         {
-            result.ModelState.AddModelError("Social", contacts.Incident.Description);
+            result.ModelState.AddModelError("Social", socials.Incident.Description);
         }
         else
         {
-            result.Data = contacts.Data ?? [];
+            result.Data = socials.Data ?? [];
         }
 
         return result;
     }
 
-    public List<PreviewSocialFeature.Response> Data { get; set; } = [];
-    public string Title => "Contacts";
+    public List<ASocialFeature.ResponsePreview> Data { get; set; } = [];
+    public string Title => "Social";
 
 
     public void OnGet()
---
+++ TLD15/TLD15/Pages/Social/_ASocialFeature.cs
@@ -0,0 +1,142 @@
+﻿using ACherryPie.Responses;
+using MediatR;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using ACherryPie.Incidents;
+using TLD15.Infrastructure;
+using TLD15.Utils;
+using TLD15.Pages.Contacts;
+
+namespace TLD15.Pages.Social;
+
+public sealed class ASocialFeature
+{
+    public sealed class RequestEdit : IRequest<ResponseId<Guid>>
+    {
+        public required Guid? Id { get; set; }
+        public required string Name { get; set; }
+        public required string Url { get; set; }
+
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime UpdatedAt { get; set; }
+        public required long Version { get; set; }
+    }
+
+    public sealed class RequestView : IRequest<List<RequestEdit>>
+    {
+        public required Guid? Id { get; set; }
+    }
+
+    public sealed class HandlerView(IMongoClient client) : IRequestHandler<RequestView, List<RequestEdit>>
+    {
+        public async Task<List<RequestEdit>> Handle(RequestView request, CancellationToken cancellationToken)
+        {
+            var database = client.GetDatabase(EntitySocial.Database);
+            var collection = database.GetCollection<EntitySocial>(EntitySocial.Collection);
+
+            var filter = request.Id.HasValue ?
+                Builders<EntitySocial>.Filter.Eq(x => x.Id, request.Id.Value)
+                : FilterDefinition<EntitySocial>.Empty;
+
+            var documents = await collection.FindAsync(filter, cancellationToken: cancellationToken);
+            var document = await documents.ToListAsync(cancellationToken);
+
+            return document.ConvertAll(x => new RequestEdit
+            {
+                Id = x.Id,
+                Name = x.Name.ToLowerInvariant(),
+                Url = x.Url.ToLowerInvariant(),
+                CreatedAt = x.CreatedAt,
+                UpdatedAt = x.UpdatedAt,
+                Version = x.Version,
+            });
+        }
+    }
+
+    public sealed class HandlerEdit(IMongoClient client) : IRequestHandler<RequestEdit, ResponseId<Guid>>
+    {
+        public async Task<ResponseId<Guid>> Handle(RequestEdit request, CancellationToken cancellationToken)
+        {
+            var database = client.GetDatabase(EntitySocial.Database);
+            var collection = database.GetCollection<EntitySocial>(EntitySocial.Collection);
+
+            var item = new EntitySocial { Id = Guid.NewGuid() };
+            if (request.Id.HasValue)
+            {
+                item = await collection.Find(x => x.Id == request.Id.Value)
+                    .FirstOrDefaultAsync(cancellationToken)
+                    ?? throw new IncidentException(IncidentCode.NotFound);
+            }
+            else
+            {
+                await collection.InsertOneAsync(item, collection.GetDefaultInsert(), cancellationToken);
+            }
+
+            item.Name = request.Name.ToLowerInvariant();
+            item.Url = request.Url.ToLowerInvariant();
+            item.Bump(request.Version);
+
+            await collection.ReplaceOneAsync(x => x.Id == item.Id, item, cancellationToken: cancellationToken);
+
+            return new ResponseId<Guid> { Id = item.Id };
+        }
+    }
+
+    public sealed class RequestDelete : IRequest<ResponseId<Guid>>
+    {
+        public required Guid Id { get; set; }
+    }
+
+
+    public sealed class HandlerDelete(IMongoClient client) : IRequestHandler<RequestDelete, ResponseId<Guid>>
+    {
+        public async Task<ResponseId<Guid>> Handle(RequestDelete request, CancellationToken cancellationToken)
+        {
+            var database = client.GetDatabase(EntitySocial.Database);
+            var collection = database.GetCollection<EntitySocial>(EntitySocial.Collection);
+
+            await collection.DeleteOneAsync(Builders<EntitySocial>.Filter.Eq(x => x.Id, request.Id), new DeleteOptions(), cancellationToken);
+
+            return new ResponseId<Guid> { Id = request.Id };
+        }
+    }
+
+    public sealed class ResponsePreview
+    {
+        public required string Url { get; set; }
+        public required string Name { get; set; }
+
+        public string Image
+        {
+            get
+            {
+                return IconHelper.GetIcon(Name);
+            }
+        }
+    }
+
+    public sealed class RequestPreview : IRequest<List<ResponsePreview>> { }
+
+    public sealed class HandlerPreview(IMongoClient client) : IRequestHandler<RequestPreview, List<ResponsePreview>>
+    {
+        public async Task<List<ResponsePreview>> Handle(RequestPreview request, CancellationToken cancellationToken)
+        {
+            var database = client.GetDatabase(EntitySocial.Database);
+            var collection = database.GetCollection<EntitySocial>(EntitySocial.Collection);
+
+            var documents = await collection.Find(FilterDefinition<EntitySocial>.Empty)
+                .SortByDescending(x => x.CreatedAt)
+                .ToListAsync(cancellationToken);
+
+            return documents.ConvertAll(x => new ResponsePreview
+            {
+                Name = x.Name,
+                Url = x.Url,
+            });
+        }
+    }
+
+}
