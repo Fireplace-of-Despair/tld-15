0eb50495a6555d11b47f235c4592787c001464e7
Chief <ChiefService@outlook.com>
2025-05-21 12:10:45 +0900

    Optimized migrator

--- TLD15/Infrastructure/Migrations/Migrator.cs
+++ TLD15/Infrastructure/Migrations/Migrator.cs
@@ -1,22 +1,23 @@
 ﻿using FluentMigrator.Runner;
 using Infrastructure.Composition;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
 
 namespace Infrastructure.Migrations;
 
 public static class Migrator
 {
+    private static readonly Lock _lock = new();
+    private static ServiceProvider? _serviceProvider;
+    private static string? _currentConnectionString;
+
     /// <summary> Migrate up to the last version </summary>
     /// <param name="connectionString"> Database connection string </param>
     public static void Up(string connectionString)
     {
-        using (var scope = CreateServices(connectionString).CreateScope())
-        {
-            var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
-
-            runner.ListMigrations();
-            runner.MigrateUp();
-        }
+        using var scope = GetOrCreateServiceProvider(connectionString).CreateScope();
+        var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+        runner.MigrateUp();
     }
 
     /// <summary> Migrate down to the given version </summary>
@@ -24,30 +25,36 @@ public static class Migrator
     /// <param name="version"> The version to migrate down to </param>
     public static void Down(string connectionString, long version)
     {
-        using (var scope = CreateServices(connectionString).CreateScope())
-        {
-            var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
-
-            runner.ListMigrations();
-            runner.MigrateDown(version);
-        }
+        using var scope = GetOrCreateServiceProvider(connectionString).CreateScope();
+        var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+        runner.MigrateDown(version);
     }
 
     /// <summary>
-    /// Creates a service provider for the migration runner
+    /// Creates or reuses a service provider for the migration runner
     /// </summary>
     /// <param name="connectionString"> Database connection string </param>
-    /// <returns> </returns>
-    private static ServiceProvider CreateServices(string connectionString)
+    private static ServiceProvider GetOrCreateServiceProvider(string connectionString)
     {
-        return new ServiceCollection()
-            .AddFluentMigratorCore()
-            .ConfigureRunner(runner => runner
-                .AddPostgres()
-                .WithGlobalConnectionString(connectionString)
-                .WithVersionTable(new VersionTable())
-                .ScanIn(typeof(Migrator).Assembly).For.Migrations())
-            .AddLogging(lb => lb.AddFluentMigratorConsole())
-            .BuildServiceProvider(false);
+        lock (_lock)
+        {
+            if (_serviceProvider == null || _currentConnectionString != connectionString)
+            {
+                _serviceProvider?.Dispose();
+
+                _serviceProvider = new ServiceCollection()
+                    .AddFluentMigratorCore()
+                    .ConfigureRunner(runner => runner
+                        .AddPostgres()
+                        .WithGlobalConnectionString(connectionString)
+                        .WithVersionTable(new VersionTable())
+                        .ScanIn(typeof(Migrator).Assembly).For.Migrations())
+                    .AddLogging(lb => lb.AddFluentMigratorConsole())
+                    .BuildServiceProvider(validateScopes: false);
+                _currentConnectionString = connectionString;
+            }
+
+            return _serviceProvider;
+        }
     }
 }
